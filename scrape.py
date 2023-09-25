1b9f18b7b1c2bf230f7002a3099c7fed27aceaaa
SsuunaFran <ssuunafran@gmail.com>
2023-09-25 13:50:49 -0400

    M2

--- scrape.py
+++ scrape.py
@@ -1,12 +1,21 @@
-import requests 
+import requests
+import mysql.connector 
 from bs4 import BeautifulSoup
 
-req=requests.get('https://www.geeksforgeeks.org/')
+req=requests.get('https://www.brightermonday.co.ug/')
 
-scrape= BeautifulSoup(req.content,"html.parser")
+scrape= BeautifulSoup(req.content,"lxml")
 
-res=scrape.a
+res=scrape.title
 
 print(res.get_text())
 
+connection=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Ancis@2001",
+    database="Scraping"
+)
+
+mycursor=connection.cursor()
 
