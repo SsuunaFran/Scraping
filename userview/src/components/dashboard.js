a62467026d2e593bf519a2acc0884b058c74ba21
SsuunaFran <ssuunafran@gmail.com>
2023-09-27 16:06:14 -0400

    Created dashboard

---
+++ userview/src/components/dashboard.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/js/bootstrap.bundle"
+class Dashboard extends Component{
+    render(){
+        return(
+            
+        )
+    }
+}
