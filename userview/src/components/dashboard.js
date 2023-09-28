61ebe4aaa9389a42184ef08c4914fb4ac5b6b0ff
SsuunaFran <ssuunafran@gmail.com>
2023-09-28 15:29:20 -0400

    created server

--- userview/src/components/dashboard.js
+++ userview/src/components/dashboard.js
@@ -1,10 +1,45 @@
 import React,{Component} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import "bootstrap/dist/js/bootstrap.bundle"
+import Piechart from './pie';
+import './mystyles.css';
+import Barchart from './bar';
 class Dashboard extends Component{
     render(){
         return(
-            
+            <React.Fragment>
+                <div className='container-fluid border border-1 border-primary'>
+                    <div className='row container-fluid m-5'>
+                        <div className='col-4'>
+                            <div className='card'>
+                                <div className='card-header'>
+                                <span className='card-title text-primary fw-bold'>Pie Chart</span>
+                                </div>
+                                <div className="card-body">
+                                    <Piechart/>
+                                </div>
+                                <div className='card-footer'>
+                                    <span className='text-primary'> Districts</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-7 m-1'>
+                            <div className='card'>
+                                <div className='card-header border border-1'>
+                                    <span className='text-primary fw-bold'>Bar Graph</span>
+                                </div>
+                                <div className='card-body'>
+                                    <Barchart/>
+                                </div>
+                                <div className="card-footer">
+                                    <span className='text-primary'>Jobs against companies</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
+
+export default Dashboard;
